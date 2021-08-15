962cfcea402d70273305ee4becf535711ca67ea9
Martin Volkmann <martin.volkmann96@gmail.com>
2021-08-15 17:36:11 +0200

    initial

---
+++ src/test/java/de/cotton/dockertest/DockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cotton.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
